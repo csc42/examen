175ab266e261482bd633f6056578b5a472dfb64d
csc42 <82498095+csc42@users.noreply.github.com>
2021-04-14 03:18:16 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,5 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyC4ZHGdWUpeSVYkoa5IstXZf_2MrLMIb24",
-    authDomain: "bdalum42.firebaseapp.com",
-    projectId: "bdalum42",
-    storageBucket: "bdalum42.appspot.com",
-    messagingSenderId: "34679409053",
-    appId: "1:34679409053:web:8ca4d76475b24d0c090774",
-    measurementId: "G-97X30Q85CH"
+    
 });
